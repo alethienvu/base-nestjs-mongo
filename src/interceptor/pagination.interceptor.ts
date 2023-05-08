e402de1463be2c1446c6648a04b3413c802160f3
alethienvu <ale.thienvu@setel.com>
2023-05-08 21:36:48 +0700

    fix: update eslint rule

--- src/interceptor/pagination.interceptor.ts
+++ src/interceptor/pagination.interceptor.ts
@@ -1,4 +1,3 @@
-// tslint:disable: no-shadowed-variable
 import { NestInterceptor, Injectable, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -10,15 +9,12 @@ export class PaginationInterceptor implements NestInterceptor {
       map((data) => {
         const headers = Object.assign({}, data.headers);
         const items = Object.assign({}, data.items);
-        // const req = context.switchToHttp().getRequest();
         for (const key in headers) {
-          if (headers.hasOwnProperty(key)) {
+          if (Object.prototype.hasOwnProperty.call(headers, key)) {
             const req = context.switchToHttp().getRequest();
             req.res.header(key, headers[key]);
           }
         }
-
-        // return values of an array
         data = Object.values(items);
         return data;
       }),
