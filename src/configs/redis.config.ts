c70a8d98edf9579cc505210d48c7508d7ce64234
alethienvu <ale.thienvu@setel.com>
2023-05-09 10:50:18 +0700

    fix: redis config

--- src/configs/redis.config.ts
+++ src/configs/redis.config.ts
@@ -1,6 +1,7 @@
 import { getConfig } from './index';
 
 export const redisConfig = {
-  host: getConfig().get<string>('redis.host'),
-  port: getConfig().get<number>('redis.port'),
+  host: getConfig().get<string>('redis.standalone.host'),
+  port: getConfig().get<number>('redis.standalone.port'),
+  pass: getConfig().get<string>('redis.standalone.pass') || '',
 };
